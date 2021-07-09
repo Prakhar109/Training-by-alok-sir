0ef9366e62be5bfad46a50dd8c90b743fd9cb9f0
Prakhar109 <prakharghatia@gmail.com>
2021-07-09 15:04:37 +0530

    save

--- app/src/main/java/com/example/training/alok/Selection.java
+++ app/src/main/java/com/example/training/alok/Selection.java
@@ -1,23 +1,24 @@
 package com.example.training.alok;
+
 import java.util.*;
+
 public class Selection {
-    public static void main(String args[])
-    {
-        Scanner sc=new Scanner(System.in);
+    public static void main(String args[]) {
+        Scanner sc = new Scanner(System.in);
         System.out.println("Enter the marks of first subject");
-        int marks1=sc.nextInt();
+        int marks1 = sc.nextInt();
         System.out.println("Enter the marks of second subject");
-        int marks2=sc.nextInt();
+        int marks2 = sc.nextInt();
         System.out.println("Enter the marks of third subject");
-        int marks3=sc.nextInt();
-        float percentage,total;
-        total=(marks1+marks2+marks3);
-        percentage=(total/300)*100;
-        if(percentage>=75)
-        {
-            System.out.println("Selected and proceed for the next round");
+        int marks3 = sc.nextInt();
+        float percentage, total;
+        total = (marks1 + marks2 + marks3);
+        percentage = (total / 300) * 100;
+        if (percentage >= 75) {
+            System.out.println("Selected and proceed for the next round " + percentage + "%");
+        } else {
+            System.out.println("Not Selected....Please try next time " + percentage + "%");
         }
-        else System.out.println("Not Selected....Please try next time");
 
     }
 }
